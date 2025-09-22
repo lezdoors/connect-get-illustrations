969873aeb6a9cc6e805be0fdfba12796fcd06ce5
lezdoors <lezdoors@users.noreply.github.com>
2025-09-22 01:06:33 +0000

    FIX: Add missing SITE import in Hero.tsx for successful build

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import MiniForm from './MiniForm'
+import { SITE } from '../config/site'
 
 const Hero: React.FC = () => {
   const navigate = useNavigate()
